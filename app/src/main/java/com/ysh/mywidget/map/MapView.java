db49fae483cea4dcd8468d674bac59d86474e1c1
SHLURENJIA <SH_LURENJIA@163.com>
2018-09-20 13:46:27 +0800

    init project

---
+++ app/src/main/java/com/ysh/mywidget/map/MapView.java
@@ -0,0 +1,30 @@
+package com.ysh.mywidget.map;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 作者：created by @author{ YSH } on 2018/9/20
+ * 描述：地图类
+ * 修改备注：
+ */
+public class MapView extends View {
+    public MapView(Context context) {
+        super(context);
+    }
+
+    public MapView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MapView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+
+
+}
